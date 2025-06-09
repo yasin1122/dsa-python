257e7ab07bd230aa0a4c4620e491b7d4571639eb
Yasin Cakal <whoisyasin@gmail.com>
2025-06-09 16:36:45 -0700

    basics

--- cheat_sheet.py
+++ cheat_sheet.py
@@ -1,1 +1,19 @@
-print("hi mom")
+# Multiple Assignments
+x, y = 10, 20
+x, y = 22, 11
+print(x, y)
+
+# None is null (absence of value)
+z = None
+
+# If statements don't need parentheses unless multi-line
+# and instead of &&, or instead of ||
+if (z == None and
+    True):
+  print("This prints")
+
+# Loops
+n = 0
+while n < 5:
+  print(n)
+  n += 1
