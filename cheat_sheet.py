cf2a856012fcd255c7518bc0b70e8474f3a4c732
Yasin Cakal <whoisyasin@gmail.com>
2025-06-09 17:05:37 -0700

    lists

--- cheat_sheet.py
+++ cheat_sheet.py
@@ -2,6 +2,7 @@
 x, y = 10, 20
 x, y = 22, 11
 print(x, y)
+x, y = [1, 2] # unpacking
 
 # None is null (absence of value)
 z = None
@@ -15,5 +16,40 @@ if (z == None and
 # Loops
 n = 0
 while n < 5:
-  print(n)
   n += 1
+
+for i in range(2, 0, -1):
+  print(i)
+
+# iterate through both index and values
+for i, num in enumerate(['a', 'b', 'c']):
+  print(i, num)
+
+# iterate through multiple arrays
+for x, y in zip([1, 2, 3], [9, 8, 7]):
+  print(x, y)
+
+# Math quirks to watch out for
+print(-3 // 2) # floor division rounds down to -2
+print(-10 % 3) # = 2, weird quirk with python
+
+# Max / Min Int
+float("inf")
+float("-inf")
+
+# Lists
+arr = [1] * 3 # initialize list with default values
+arr.append(4)
+arr.pop()
+arr.insert(1, 5)
+print(arr, len(arr), arr[1:3])
+arr.sort(reverse=True) # sort or reverse sort in place
+arr.reverse() # just reverse in place
+print(arr)
+arr = ["bob", "alice", "jane", "doe"]
+arr.sort(key=lambda x: len(x))
+print(arr)
+arr = [i*2 for i in range(5)] # List comprehension
+print(arr)
+arr = [[i] * 4 for i in range(4)] # 2-D list comprehension
+print (arr) # arr = [[0] * 4] * 4 won't work
